c12b124c31aba29baae6e4989bf627991eb7ef84
Kevin <hoangcuongvnit@gmail.com>
2025-10-22 23:24:19 +0700

    Update navbar branding and improve import organization in main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ export default function App() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <a className="navbar-brand" href="#">LearningApp</a>
+          <a className="navbar-brand" href="#">Learning</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
